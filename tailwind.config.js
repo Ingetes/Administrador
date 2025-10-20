3c2bedea4aa535e723202e8f27e2ff5ad4d3f732
Ingetes <comercial@ingetes.com>
2025-10-20 11:08:17 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,14 @@ export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: { extend: {} },
   plugins: [],
+{
+  "devDependencies": {
+    "tailwindcss": "^3",
+    "postcss": "^8",
+    "autoprefixer": "^10",
+    "vite": "^5",
+    "@vitejs/plugin-react": "^4"
+  }
+}
+
 }
