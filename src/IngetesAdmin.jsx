edd934249179577ea8c86776eb617fddfce8d7b4
Ingetes <comercial@ingetes.com>
2025-10-20 12:20:38 -0500

    Update IngetesAdmin.jsx

--- src/IngetesAdmin.jsx
+++ src/IngetesAdmin.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState, createContext, useContext } from "react";
 
-const logoIngetes = "https://ingetes.github.io/Portal-de-clientes/ingetes.jpg";
+const logoIngetes = "https://ingetes.github.io/Portal-de-clientes/public/ingetes.jpg";
 
 /** ========== Mock de usuarios (SOLO DEMO) ========== */
 const USERS = [
