e4bb511f1bc28e716d687631c8b65f8eec72b45c
Ingetes <comercial@ingetes.com>
2025-10-20 10:52:30 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import IngetesAdmin from "./IngetesAdmin.jsx";
+import "./index.css"; // si usas Tailwind
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <IngetesAdmin />
+  </React.StrictMode>
+);
 
