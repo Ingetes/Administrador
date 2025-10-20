661263d1345b23f5e6cd1051e41c6e8bbfcc2da9
Ingetes <comercial@ingetes.com>
2025-10-20 11:06:51 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Administrador/', // 👈 nombre EXACTO del repo en GitHub
+  base: '/Administrador/', // nombre EXACTO del repo
 })
