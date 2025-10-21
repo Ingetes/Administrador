c5616568b0a22db3f60301b7dc4e78bc9cd4f977
Ingetes <comercial@ingetes.com>
2025-10-21 07:18:58 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import IngetesAdmin from './IngetesAdmin.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import IngetesAdmin from "./IngetesAdmin.jsx";
+import Portaladmin from "./Portaladmin.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <IngetesAdmin />
-  </React.StrictMode>
-)
+const root = ReactDOM.createRoot(document.getElementById("root")); // ← solo una vez
+
+function renderRoute() {
+  const hash = window.location.hash.replace("#", "");
+  if (hash === "/portal_admin") {
+    root.render(<Portaladmin />);           // ← re-renderiza, desmonta la vista previa
+  } else {
+    root.render(<IngetesAdmin />);
+  }
+}
+
+window.addEventListener("hashchange", renderRoute);
+renderRoute(); // primera carga
